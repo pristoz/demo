fa4a0de232ea695ee3c63e848e70187b5aa109f2
Носырев Денис Алексеевич <DNosyrev@itcorp.it.ru>
2018-12-13 21:40:57 +0300

    add project

---
+++ CalculatingAreaOfFigures/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CalculatingAreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         // тест
+           /*
+            var areaCircle =  new Circle(2).Area();
+            var areaTriangle = new Triangle(21, 33, 44).Area();
+             */
+        }
+    }
+}
