d1e79789d1e097ea0931fbeda98bb6d1f120f24c
Носырев Денис Алексеевич <DNosyrev@itcorp.it.ru>
2018-12-28 00:14:25 +0300

    БД

---
+++ CalculatingAreaOfFigures/CalculatingAreaOfFigures/Figures/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CalculatingAreaOfFigures
+{
+   public class Circle:IFigureArea
+    {
+        double r; //-- радиус круга
+        public Circle(double r)
+        {
+            this.r = r;
+        }
+        public double Area()
+        {
+            return Math.PI * Math.Pow(r, 2); //-- формула пл-ди круга
+        }
+    }
+}
---
+++ CalculatingAreaOfFigures/CalculatingAreaOfFigures/Figures/Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalculatingAreaOfFigures
+{
+  public class Triangle:IFigureArea
+    {
+        double a, b, c; //-- стороны треугольника
+       
+        public Triangle( double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public double Area()
+        {
+            var p = (a + b + c) / 2; //-- полупериметр треугольника
+
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c)); //-- формула площади треугольника по трем сторонам
+        }
+    }
+}
